2fab90090f736986f6ca2b98f030d9cf3874cc7a
Luis Felipe Nunes <lipenunes2006@gmail.com>
2024-10-27 15:33:39 -0300

    Conectei com o meu container no docker e dbeaver

---
+++ src/main/groovy/com/example/SPTJ_BD/SptjBdApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.SPTJ_BD
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SptjBdApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SptjBdApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/example/SPTJ_BD/SptjBdApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.example.SPTJ_BD
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SptjBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
